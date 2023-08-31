7226ec7079633159296e3c4933ad635b0a6ba6e0
codernoyan <coder.noyan@gmail.com>
2023-08-31 22:51:42 +0600

    folder structure organized

--- src/routes/router/Router.jsx
+++ src/routes/router/Router.jsx
@@ -2,6 +2,7 @@
 import { createBrowserRouter } from 'react-router-dom';
 import Dashboard from '../../layouts/Dashboard';
 import Main from '../../layouts/Main';
+import Summary from '../../pages/daashboard/summary/Summary';
 import Home from '../../pages/home/Home';
 import NotFound from '../../pages/notFound/NotFound';
 
@@ -19,13 +20,13 @@ export const router = createBrowserRouter([
   },
   {
     path: '/dashboard',
-    element: <Dashboard/>,
+    element: <Dashboard />,
     errorElement: <NotFound />,
     children: [
       {
         path: '/dashboard',
-        element: ,
-      }
-    ]
+        element: <Summary />,
+      },
+    ],
   },
 ]);
