07589a1f0db5e42448cfc719123b2cc7ef5bd50c
codernoyan <coder.noyan@gmail.com>
2023-09-01 03:24:38 +0600

    added footer

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,10 +1,7 @@
-import Button from '../../ui/button/Button';
-
 export default function Home() {
   return (
     <main>
       <h2>home</h2>
-      <Button className="button-press ml-2">Clickable</Button>
     </main>
   );
 }
