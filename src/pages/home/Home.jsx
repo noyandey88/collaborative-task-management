0454549331f5badf27ef1855c5c1b6ade679a663
codernoyan <coder.noyan@gmail.com>
2023-09-01 09:47:07 +0600

    feat: added get organized section

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,9 +1,11 @@
+import GetOrganized from '../../components/getOrganized/GetOrganized';
 import Hero from '../../components/hero/Hero';
 
 export default function Home() {
   return (
     <main>
       <Hero />
+      <GetOrganized />
     </main>
   );
 }
