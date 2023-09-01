16cfd1c13905f82a68f861fc4a6584a25696212a
codernoyan <coder.noyan@gmail.com>
2023-09-01 13:34:28 +0600

    functionality added to login page

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -155,20 +155,39 @@ export default function Navbar() {
 
           <Disclosure.Panel className="sm:hidden">
             <div className="space-y-1 px-2 pb-3 pt-2">
-              {navigation.map((item) => (
-                <Disclosure.Button
-                  key={item.name}
-                  as="a"
-                  href={item.href}
-                  className={classNames(
-                    item.current ? 'bg-secondary text-primary' : 'text-secondary hover:bg-secondary hover:text-primary',
-                    'block rounded-md px-3 py-2 text-base font-medium',
-                  )}
-                  aria-current={item.current ? 'page' : undefined}
-                >
-                  {item.name}
-                </Disclosure.Button>
-              ))}
+              {
+                !user?.uid
+                  ? navigation.filter((item) => item.name !== 'Dashboard')
+                    .map((item) => (
+                      <Disclosure.Button
+                        key={item.name}
+                        as="a"
+                        href={item.href}
+                        className={classNames(
+                          item.current ? 'bg-secondary text-primary' : 'text-secondary hover:bg-secondary hover:text-primary',
+                          'block rounded-md px-3 py-2 text-base font-medium',
+                        )}
+                        aria-current={item.current ? 'page' : undefined}
+                      >
+                        {item.name}
+                      </Disclosure.Button>
+                    )) : (
+                    navigation.map((item) => (
+                      <Disclosure.Button
+                        key={item.name}
+                        as="a"
+                        href={item.href}
+                        className={classNames(
+                          item.current ? 'bg-secondary text-primary' : 'text-secondary hover:bg-secondary hover:text-primary',
+                          'block rounded-md px-3 py-2 text-base font-medium',
+                        )}
+                        aria-current={item.current ? 'page' : undefined}
+                      >
+                        {item.name}
+                      </Disclosure.Button>
+                    ))
+                  )
+              }
             </div>
           </Disclosure.Panel>
         </>
--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -1,8 +1,37 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { useContext } from 'react';
+import { useForm } from 'react-hook-form';
+import { toast } from 'react-hot-toast';
+import { Link, useLocation, useNavigate } from 'react-router-dom';
+import { AuthContext } from '../../contexts/AuthProvider';
 import Button from '../../ui/button/Button';
 
 export default function Login() {
+  const {
+    loginUser, loading, setLoading,
+  } = useContext(AuthContext);
+  const { register, formState: { errors }, handleSubmit } = useForm();
+  const navigate = useNavigate();
+  const location = useLocation();
+  const from = location?.state?.from?.pathname || '/';
+
+  const handleLoginUser = (data) => {
+    const { email } = data;
+    const { password } = data;
+    loginUser(email, password)
+      .then((result) => {
+        const { user } = result;
+        console.log(user);
+        toast.success('Login Successful');
+        setLoading(false);
+        navigate(from, { replace: true });
+      })
+      .catch((error) => {
+        console.error(error);
+        toast.error(error.message);
+        setLoading(false);
+      });
+  };
+
   return (
     <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
       <div className="sm:mx-auto sm:w-full sm:max-w-sm">
@@ -14,22 +43,27 @@ export default function Login() {
       </div>
 
       <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
-        <form className="space-y-6">
+        <form onSubmit={handleSubmit(handleLoginUser)} className="space-y-4">
+          {/* email */}
           <div>
-            <label htmlFor="email1" className="block text-sm font-medium leading-6 text-gray-900">
+            <label htmlFor="email" className="block text-sm font-medium leading-6 text-gray-900">
               Email address
             </label>
             <div className="mt-2">
               <input
-                id="email1"
+                id="email"
                 name="email"
                 type="email"
                 autoComplete="email"
-                required
                 className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-dark sm:text-sm sm:leading-6"
+                {...register('email', {
+                  required: 'Email is required',
+                })}
               />
+              {errors.email && <p className="mt-1 text-red-500 font-medium">{errors?.email.message}</p>}
             </div>
           </div>
+          {/* password */}
           <div>
             <div className="flex items-center justify-between">
               <label htmlFor="password" className="block text-sm font-medium leading-6 text-gray-900">
@@ -47,18 +81,33 @@ export default function Login() {
                 name="password"
                 type="password"
                 autoComplete="current-password"
-                required
                 className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-dark sm:text-sm sm:leading-6"
+                {...register('password', {
+                  required: 'Password is Required',
+                  minLength: { value: 6, message: 'Password must be 6 characters or longer' },
+                })}
               />
+              {errors.password && <p className="mt-1 text-red-500 font-medium">{errors.password.message}</p>}
             </div>
           </div>
-
           <div>
             <Button
               type="submit"
               className="flex w-full justify-center rounded-md px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-dark focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2"
+              disabled={loading}
             >
-              Sign in
+              {
+                !loading
+                  ? 'Login'
+                  : (
+                    <span className="flex items-center justify-center space-x-2 py-2">
+                      <span className="w-2 h-2 rounded-full animate-pulse bg-secondary" />
+                      <span className="w-2 h-2 rounded-full animate-pulse bg-secondary" />
+                      <span className="w-2 h-2 rounded-full animate-pulse bg-secondary" />
+                      <span className="w-2 h-2 rounded-full animate-pulse bg-secondary" />
+                    </span>
+                  )
+              }
             </Button>
           </div>
         </form>
