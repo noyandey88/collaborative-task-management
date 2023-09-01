3439c30c9da4ddbdad1cdea7dd03714617d65f1b
codernoyan <coder.noyan@gmail.com>
2023-09-01 11:03:55 +0600

    register page re-designed

--- src/pages/register/Register.jsx
+++ src/pages/register/Register.jsx
@@ -14,12 +14,43 @@ export default function Register() {
       </div>
 
       <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
-        <form className="space-y-6">
+        <form className="space-y-4">
+          {/* username */}
+          <div>
+            <label htmlFor="username" className="block text-sm font-medium leading-6 text-gray-900">
+              Username
+            </label>
+            <div className="mt-1">
+              <input
+                id="username"
+                name="username"
+                type="text"
+                autoComplete="username"
+                required
+                className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-dark sm:text-sm sm:leading-6"
+              />
+            </div>
+          </div>
+          {/* user profile photo */}
+          <div>
+            <label htmlFor="profile__photo" className="block text-sm font-medium leading-6 text-gray-900">
+              Profile photo
+            </label>
+            <div className="mt-1">
+              <input
+                id="profile__photo"
+                type="file"
+                className="block w-full text-sm rounded-md text-slate-500 file:mr-4 file:py-2 file:px-4 border-2 file:border-0 file:text-sm file:font-semibold file:bg-primary file:text-secondary hover:file:bg-dark"
+                accept="image/*"
+              />
+            </div>
+          </div>
+          {/* email */}
           <div>
             <label htmlFor="email1" className="block text-sm font-medium leading-6 text-gray-900">
               Email address
             </label>
-            <div className="mt-2">
+            <div className="mt-1">
               <input
                 id="email1"
                 name="email"
@@ -30,13 +61,14 @@ export default function Register() {
               />
             </div>
           </div>
+          {/* password */}
           <div>
             <div className="flex items-center justify-between">
               <label htmlFor="password" className="block text-sm font-medium leading-6 text-gray-900">
                 Password
               </label>
             </div>
-            <div className="mt-2">
+            <div className="mt-1">
               <input
                 id="password"
                 name="password"
@@ -47,7 +79,7 @@ export default function Register() {
               />
             </div>
           </div>
-
+          {/* submit button */}
           <div>
             <Button
               type="submit"
@@ -57,7 +89,6 @@ export default function Register() {
             </Button>
           </div>
         </form>
-
         <p className="mt-10 text-center text-sm text-gray-500">
           Already a member?
           {' '}
