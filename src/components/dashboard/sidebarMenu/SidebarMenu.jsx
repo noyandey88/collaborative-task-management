4cb1aee74de8ca0f85cd650015897e28468f0956
codernoyan <coder.noyan@gmail.com>
2023-09-01 17:44:16 +0600

    fix: dashboard is breaked into smaller parts

---
+++ src/components/dashboard/sidebarMenu/SidebarMenu.jsx
@@ -0,0 +1,94 @@
+import { ArrowUturnLeftIcon } from '@heroicons/react/20/solid';
+import { Link } from 'react-router-dom';
+
+export default function SidebarMenu() {
+  return (
+    <nav className="hs-accordion-group p-6 w-full flex flex-col flex-wrap" data-hs-accordion-always-open>
+      <ul className="mt-6 space-y-2">
+        <li>
+          <Link to="/dashboard" className="flex items-center gap-x-2 rounded-md bg-secondary px-4 py-2 text-sm font-medium text-primary">
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+              <path strokeLinecap="round" strokeLinejoin="round" d="M8.25 21v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21m0 0h4.5V3.545M12.75 21h7.5V10.75M2.25 21h1.5m18 0h-18M2.25 9l4.5-1.636M18.75 3l-1.5.545m0 6.205l3 1m1.5.5l-1.5-.5M6.75 7.364V3h-3v18m3-13.636l10.5-3.819" />
+            </svg>
+            Dashboard
+          </Link>
+        </li>
+        {/* my tasks */}
+        <li>
+          <Link to="/dashboard/my-tasks" className="flex items-center gap-x-2 rounded-lg px-4 py-2 text-sm font-medium text-secondary hover:bg-secondary hover:text-primary">
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+              <path strokeLinecap="round" strokeLinejoin="round" d="M6 6.878V6a2.25 2.25 0 012.25-2.25h7.5A2.25 2.25 0 0118 6v.878m-12 0c.235-.083.487-.128.75-.128h10.5c.263 0 .515.045.75.128m-12 0A2.25 2.25 0 004.5 9v.878m13.5-3A2.25 2.25 0 0119.5 9v.878m0 0a2.246 2.246 0 00-.75-.128H5.25c-.263 0-.515.045-.75.128m15 0A2.25 2.25 0 0121 12v6a2.25 2.25 0 01-2.25 2.25H5.25A2.25 2.25 0 013 18v-6c0-.98.626-1.813 1.5-2.122" />
+            </svg>
+            <span>My Tasks</span>
+          </Link>
+        </li>
+        {/* projects */}
+        <li>
+          <details className="group [&_summary::-webkit-details-marker]:hidden">
+            <summary className="flex cursor-pointer items-center justify-between rounded-lg px-4 py-2 text-secondary hover:bg-secondary hover:text-primary bg-dark">
+              <span className="text-sm font-medium flex items-center gap-x-2">
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+                  <path strokeLinecap="round" strokeLinejoin="round" d="M9 12h3.75M9 15h3.75M9 18h3.75m3 .75H18a2.25 2.25 0 002.25-2.25V6.108c0-1.135-.845-2.098-1.976-2.192a48.424 48.424 0 00-1.123-.08m-5.801 0c-.065.21-.1.433-.1.664 0 .414.336.75.75.75h4.5a.75.75 0 00.75-.75 2.25 2.25 0 00-.1-.664m-5.8 0A2.251 2.251 0 0113.5 2.25H15c1.012 0 1.867.668 2.15 1.586m-5.8 0c-.376.023-.75.05-1.124.08C9.095 4.01 8.25 4.973 8.25 6.108V8.25m0 0H4.875c-.621 0-1.125.504-1.125 1.125v11.25c0 .621.504 1.125 1.125 1.125h9.75c.621 0 1.125-.504 1.125-1.125V9.375c0-.621-.504-1.125-1.125-1.125H8.25zM6.75 12h.008v.008H6.75V12zm0 3h.008v.008H6.75V15zm0 3h.008v.008H6.75V18z" />
+                </svg>
+                Projects
+              </span>
+              <span className="shrink-0 transition duration-300 group-open:-rotate-180">
+                <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
+                  <path fillRule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clipRule="evenodd" />
+                </svg>
+              </span>
+            </summary>
+            <ul className="mt-2 space-y-1 px-4">
+              <li>
+                <a href className="block rounded-lg px-4 py-2 text-sm font-medium text-secondary hover:bg-secondary hover:text-primary">
+                  Details
+                </a>
+              </li>
+              <li>
+                <a href className="block rounded-lg px-4 py-2 text-sm font-medium text-secondary hover:bg-secondary hover:text-primary">
+                  Security
+                </a>
+              </li>
+            </ul>
+          </details>
+        </li>
+        {/* teams */}
+        <li>
+          <details className="group [&_summary::-webkit-details-marker]:hidden">
+            <summary className="flex cursor-pointer items-center justify-between rounded-lg px-4 py-2 text-secondary hover:bg-secondary hover:text-primary bg-dark">
+              <span className="text-sm font-medium flex items-center gap-x-2">
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+                  <path strokeLinecap="round" strokeLinejoin="round" d="M18 18.72a9.094 9.094 0 003.741-.479 3 3 0 00-4.682-2.72m.94 3.198l.001.031c0 .225-.012.447-.037.666A11.944 11.944 0 0112 21c-2.17 0-4.207-.576-5.963-1.584A6.062 6.062 0 016 18.719m12 0a5.971 5.971 0 00-.941-3.197m0 0A5.995 5.995 0 0012 12.75a5.995 5.995 0 00-5.058 2.772m0 0a3 3 0 00-4.681 2.72 8.986 8.986 0 003.74.477m.94-3.197a5.971 5.971 0 00-.94 3.197M15 6.75a3 3 0 11-6 0 3 3 0 016 0zm6 3a2.25 2.25 0 11-4.5 0 2.25 2.25 0 014.5 0zm-13.5 0a2.25 2.25 0 11-4.5 0 2.25 2.25 0 014.5 0z" />
+                </svg>
+                Teams
+              </span>
+              <span className="shrink-0 transition duration-300 group-open:-rotate-180">
+                <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
+                  <path fillRule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clipRule="evenodd" />
+                </svg>
+              </span>
+            </summary>
+            <ul className="mt-2 space-y-1 px-4">
+              <li>
+                <a href className="block rounded-lg px-4 py-2 text-sm font-medium text-secondary hover:bg-secondary hover:text-primary">
+                  Details
+                </a>
+              </li>
+              <li>
+                <a href className="block rounded-lg px-4 py-2 text-sm font-medium text-secondary hover:bg-secondary hover:text-primary">
+                  Security
+                </a>
+              </li>
+            </ul>
+          </details>
+        </li>
+        <li>
+          <Link to="/" className="flex gap-x-2 items-center mt-10 justify-between rounded-lg px-4 py-2 text-sm font-medium text-white hover:bg-secondary hover:text-primary">
+            <span>Back to Homepage</span>
+            <ArrowUturnLeftIcon className="w-4 h-4" />
+          </Link>
+        </li>
+      </ul>
+    </nav>
+  );
+}
