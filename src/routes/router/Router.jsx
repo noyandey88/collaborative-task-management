195349f3dc25db3178b273a0e470ed28cc398800
codernoyan <coder.noyan@gmail.com>
2023-09-01 09:19:14 +0600

    feat: added dashboard pages in route

--- src/routes/router/Router.jsx
+++ src/routes/router/Router.jsx
@@ -2,9 +2,14 @@
 import { createBrowserRouter } from 'react-router-dom';
 import Dashboard from '../../layouts/Dashboard';
 import Main from '../../layouts/Main';
+import MyTasks from '../../pages/daashboard/myTasks/MyTasks';
+import Projects from '../../pages/daashboard/projects/Projects';
 import Summary from '../../pages/daashboard/summary/Summary';
+import Teams from '../../pages/daashboard/teams/Teams';
 import Home from '../../pages/home/Home';
+import Login from '../../pages/login/Login';
 import NotFound from '../../pages/notFound/NotFound';
+import Register from '../../pages/register/Register';
 
 export const router = createBrowserRouter([
   {
@@ -16,6 +21,14 @@ export const router = createBrowserRouter([
         path: '/',
         element: <Home />,
       },
+      {
+        path: '/login',
+        element: <Login />,
+      },
+      {
+        path: '/register',
+        element: <Register />,
+      },
     ],
   },
   {
@@ -26,6 +39,18 @@ export const router = createBrowserRouter([
       {
         path: '/dashboard',
         element: <Summary />,
+      },
+      {
+        path: '/dashboard/my-tasks',
+        element: <MyTasks />,
+      },
+      {
+        path: '/dashboard/projects',
+        element: <Projects />,
+      },
+      {
+        path: '/dashboard/teams',
+        element: <Teams />,
       },
     ],
   },
