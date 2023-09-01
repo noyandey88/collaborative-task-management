7ed5a5ef87f952a1d28cbfcac3c6e69f2e1ce2ce
codernoyan <coder.noyan@gmail.com>
2023-09-01 20:06:05 +0600

    feat: create button add for small device

--- src/components/dashboard/create/Create.jsx
+++ src/components/dashboard/create/Create.jsx
@@ -1,16 +1,18 @@
 import { PlusCircleIcon } from '@heroicons/react/20/solid';
 import Button from '../../../ui/button/Button';
 import ProjectModal from '../modals/projectModal/ProjectModal';
+import TaskModal from '../modals/taskModal/TaskModal';
 import TeamModal from '../modals/teamModal/TeamModal';
 
 export default function Create() {
   return (
     <div className="hs-dropdown relative inline-flex">
-      <Button id="hs-dropdown-basic" type="button" className="bg-secondary text-dark font-bold py-1 -mt-2 flex items-center gap-x-2">
+      <Button id="hs-dropdown-basic" type="button" className="bg-secondary text-dark font-bold py-1 flex items-center gap-x-2">
         <span>Create</span>
         <PlusCircleIcon className="w-6 h-6" />
       </Button>
       <div className="hs-dropdown-menu transition-[opacity,margin] duration hs-dropdown-open:opacity-100 opacity-0 w-56 hidden z-10 mt-2 min-w-[15rem] bg-secondary shadow-md rounded-lg p-2" aria-labelledby="hs-dropdown-basic">
+        <TaskModal />
         <ProjectModal />
         <TeamModal />
       </div>
--- src/layouts/Dashboard.jsx
+++ src/layouts/Dashboard.jsx
@@ -50,19 +50,9 @@ export default function Dashboard() {
             </svg>
           </button>
           {/* End Navigation Toggle */}
-          {/* Breadcrumb */}
-          <ol className="ml-3 flex items-center whitespace-nowrap min-w-0" aria-label="Breadcrumb">
-            <li className="flex items-center text-sm text-dark">
-              Dashboard Layout
-              <svg className="flex-shrink-0 mx-3 overflow-visible h-2.5 w-2.5 text-dark" width={16} height={16} viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
-                <path d="M5 1L10.6869 7.16086C10.8637 7.35239 10.8637 7.64761 10.6869 7.83914L5 14" stroke="currentColor" strokeWidth={2} strokeLinecap="round" />
-              </svg>
-            </li>
-            <li className="text-sm font-semibold text-dark truncate" aria-current="page">
-              Dashboard
-            </li>
-          </ol>
-          {/* End Breadcrumb */}
+          <div className="border-2 border-primary ml-4 rounded-md">
+            <Create />
+          </div>
         </div>
       </div>
       {/* End Sidebar Toggle */}
